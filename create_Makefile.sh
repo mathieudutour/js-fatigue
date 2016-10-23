4bf04d6ba06e8c8e98e72e7df42cebca404a17da
Mathieu Dutour <mathieu@dutour.me>
2016-10-23 16:16:24 +0100

    upadte deps

---
+++ create_Makefile.sh
@@ -0,0 +1,9 @@
+cat > ../../../Makefile << EOF
+BUILD_DIST ?= build
+BUILD_TARGET ?= src/
+BUILD_FLAGS ?= --out-dir $(BUILD_DIST)
+
+TEST_TARGET ?= tests/
+TEST_FLAGS ?= --require babel-register
+
+include node_modules/@mathieudutour/js-fatigue/Makefile
